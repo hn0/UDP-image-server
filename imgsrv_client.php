a15f4fb2dd8f1dbb92b973744c2af79c8300faff
Hrvoje Novosel <hrvojedotnovosel@gmail.com>
2018-02-24 10:49:51 +0100

    * refractoring old test server script

--- imgsrv_client.php
+++ imgsrv_client.php
@@ -11,24 +11,32 @@ class NetImage
     public static $IMG_BUFF = 2048;
     private $server;
     private $port;
-
     private $image;
+    private $log_fnc;
 
-    public function __construct( $server='localhost', $port=20000 )
+    public function __construct( $server='localhost', $port=20000, $log_fnc=null )
     {
-        $this->server = $server;
-        $this->port   = $port;
-
+        $this->server  = $server;
+        $this->port    = $port;
+        $this->log_fnc = $log_fnc;
     }
 
     public function GetImage( $path, $w, $h )
     {
         if( ($sock = socket_create( AF_INET, SOCK_DGRAM, 0)) ) {
+            $this->log( sprintf( 'Server msg: w:%d, h:%d, path:%s', $w, $h, $path ) );
             $msg = pack( 'NN', $w, $h ) . $path;
             if( socket_sendto( $sock, $msg, strlen($msg), 0, $this->server, $this->port ) ){
+            $this->log( sprintf( 'Socket is open now. Sending msg to: to %s:%s', $this->server, $this->port ) );
                 $bytes;
                 while( socket_recv( $sock, $bytes, self::$IMG_BUFF, MSG_WAITALL ) == self::$IMG_BUFF ){
                     $this->image .= $bytes;
+                }
+                if( $this->image && $this->image != -1 ){
+                    $this->log( sprintf( 'Got response of size:', strlen($this->image) ) );
+                }
+                else{
+                    $this->log( 'Request failed' );
                 }
             }
         }
@@ -41,6 +49,21 @@ class NetImage
             return base64_encode( $this->image );
         }
         return '';
+    }
+
+    public function Blob()
+    {
+        if( isset($this->image) ){
+            return $this->image;
+        }
+        return '';
+    }
+
+    private function log($msg)
+    {
+        if( $this->log_fnc ){
+            call_user_func( $this->log_fnc, $msg );
+        }
     }
 
 }
--- test_server.php
+++ test_server.php
@@ -5,6 +5,7 @@
 
 */
 
+require( 'imgsrv_client.php' );
 define( 'SERVER', 'localhost' );
 define( 'PORT', 20000 );
 define( 'THUMB_WIDTH', 800 );
@@ -13,42 +14,23 @@ function println( $msg ) {
     print( $msg . "\n" );
 }
 
-$tst_path = '/test_img/DJI_0510.JPG';
+$tst_path = 'img/DJI_0510.JPG';
 $out_path = 'test.jpg';
 
 if( defined('STDIN') && sizeof($argv) > 1 ){
-    $out_path = $argv[1];    
+    $tst_path = $argv[1];    
 }
 
-
-if( ($sock = socket_create( AF_INET, SOCK_DGRAM, 0) ) ){
-
-    // headers as follows: UINT32 UINT32 CHAR[] (width, height, path)
-    $msg = pack( 'NN', THUMB_WIDTH, THUMB_WIDTH * .75 ) . $tst_path;
-    // $msg ='';
-
-    println( sprintf('Request values %d, %d, %s', THUMB_WIDTH, THUMB_WIDTH * .75, $tst_path ) );
-    
-    $reqmsg = 'opening server request ... ';
-    $res = socket_sendto($sock, $msg, strlen($msg), 0, SERVER, PORT);
-    $reqmsg .= ($res) ? 'OK' : 'error';
-    println( $reqmsg );
-
-    $image = '';
-    $response;
-    println( 'listening server response ... ' );
-    while( ( $bytes = socket_recv( $sock, $response, 2048, MSG_WAITALL ) ) == 2048 ){
-        $image .=  $response;
-    };
-    if( $response == '-1' ){
-        println( 'got -1 response' );
-    }
-    else {
-        println( 'done' );
-        file_put_contents('test_results/' . $out_path, $image);
-    }
-    
+if( defined('STDIN') && sizeof($argv) > 2 ){
+    $out_path = $argv[2];    
 }
 
+$netimage = new NetImage( SERVER, PORT, println );
+$start = microtime(true);
+if( $netimage->GetImage( $tst_path, THUMB_WIDTH, THUMB_WIDTH * .75 ) ){
+    file_put_contents('test_results/' . $out_path, $netimage->Blob() );
+}
+
+println( sprintf( "Request time: %f", (microtime(true) - $start) ) );
 
 ?>
